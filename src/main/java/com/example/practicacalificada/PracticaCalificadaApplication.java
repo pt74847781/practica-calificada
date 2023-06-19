271ce5566d6f225a3318964972eb44c507f699f7
pt74847781 <ariel_sc_2001@hotmail.com>
2023-06-19 13:20:24 -0500

    first commit

---
+++ src/main/java/com/example/practicacalificada/PracticaCalificadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicacalificada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaCalificadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaCalificadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practicacalificada/PracticaCalificadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicacalificada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaCalificadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
